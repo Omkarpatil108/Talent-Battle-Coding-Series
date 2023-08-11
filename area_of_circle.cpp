38f52b16e5774130e9686d9b739c90335d2d0e5a
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:35:12 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius;
+    cout<<"Enter radius";
+    cin>>radius;
+    float ans=3.14*radius*radius;
+    cout<<"Area of circle is "<<ans<<endl;
+    return 0;
+
+}
