9b3d21c372be332b22cd0b0150a6f9ecbfc179c1
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:57:59 +0530

    Add files via upload

---
+++ positive_or_negative.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter number";
+    cin>>num;
+    if(num<0) cout<<"number is negative"<<endl;
+    else cout<<"number is positive"<<endl;
+    return 0;
+}
