de1b55c5bb41bdc077e409a0ecd4faee2373e0bc
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:59:37 +0530

    Add files via upload

---
+++ even_or_odd.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int number;
+    cout<<"Enter number";
+    cin>>number;
+    if(number%2==0) cout<<"Even";
+    else cout<<"Odd";
+    return 0;
+}
