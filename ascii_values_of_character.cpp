1f12ffffdac4e9b529174582d43dcc64a3afec3d
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:56:22 +0530

    Add files via upload

---
+++ ascii_values_of_character.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"Enter charcter";
+    cin>>c;
+
+
+    // if(c >=65 && c<=90)
+    // {
+    //     int ans=c+65;
+    //     cout<<"The value is "<<ans<<endl;
+    // }
+    // else if(c>=97 && c<=122)
+    // {
+    //     int ans=c+97; 
+    //     cout<<"The value is "<<ans<<endl;
+    // }
+
+    cout<<"The ASCII value of this is "<<(int)c<<endl;
+    
+    
+    return 0;
+}
