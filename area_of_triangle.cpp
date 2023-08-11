01b65ceeb134317073ed12a69864502012f298d4
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:36:23 +0530

    Add files via upload

---
+++ area_of_triangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int base,height;
+    cout<<"Enter base";
+    cin>>base;
+    cout<<"Enter height";
+    cin>>height;
+    int area=(base*height)/2;
+    cout<<"Area of triangle is "<<area<<endl;
+    return 0;
+}
