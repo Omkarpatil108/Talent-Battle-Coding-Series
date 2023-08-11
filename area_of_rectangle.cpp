fdaead139bc99a28aaecb84d5e4e2c41e37ce2e0
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-08-11 22:35:49 +0530

    Add files via upload

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int length,breadth;
+    cout<<"Enter length"<<endl;
+    cin>>length;
+    cout<<"Enter breadth"<<endl;
+    cin>>breadth;
+
+    int area=length*breadth;
+    cout<<"Area of rectangle is "<<area<<endl;
+    return 0;
+}
