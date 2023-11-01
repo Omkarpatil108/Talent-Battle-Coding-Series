d9e4dfc2cc0c82337733f421fef3ef97fc677f9e
Omkar Patil <91424350+Omkarpatil108@users.noreply.github.com>
2023-11-01 23:43:18 +0530

    Add files via upload

---
+++ factorialusingRECURSION.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+ int fact(int n)
+ {
+    if(n==0)
+    {
+        return 1;
+    }
+    else{
+        return fact(n-1)*n;
+    }
+
+ }
+ int main()
+ {
+    int n;
+    cin>>n;
+    int total=fact(n);
+    cout<<total;
+ }
